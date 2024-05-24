b99e298796260d117d1b0dea432ad18d696bef2a
Daniel Szabo <d.g.szabo-2017@hull.ac.uk>
2024-05-24 15:34:13 +0100

    Add cookie consent banner and functionality

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+function acceptCookies() {
+  cookiesAccepted = true;
+  console.log("Cookies accepted!");
+  document.querySelector(".cookie").style.display = "none";
+}
+
+let cookiesAccepted = false;
+
+const acceptCookiesButton = document.getElementById("accept-cookies");
+
+acceptCookiesButton.addEventListener("click", acceptCookies);
